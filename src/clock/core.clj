a3a7fd558207cd969db768c1e9683d35e68660d3
mark <mark@entro-1>
2017-09-11 20:39:21 +0100

    daily commit

--- src/clock/core.clj
+++ src/clock/core.clj
@@ -1,46 +1,38 @@
 (ns clock.core
   (:gen-class))
 
+(def tens-words {1 "ten"
+                 2 "twenty"
+                 3 "thirty"})
 
-(def tens-words {
-           1 "ten"
-           2 "twenty"
-           3 "thirty"
-                 })
-
-(def small-words {
-                  0 "zero"
-           1 "one"
-           2 "two"
-            3 "three"
-            4 "four"
-            5 "five"
-            6 "six"
-            7 "seven"
-            8 "eight"
-            9 "nine"
-            10 "ten"
-            11 "eleven"
-            12 "twelve"
-            13 "thirteen"
-            14 "fourteen"
-            15 "fifteen"
-            16 "sixteen"
-            17 "seventeen"
-            18 "eighteen"
-            19 "nineteen"
-            })
+(def small-words {0 "zero"
+                  1 "one"
+                  2 "two"
+                  3 "three"
+                  4 "four"
+                  5 "five"
+                  6 "six"
+                  7 "seven"
+                  8 "eight"
+                  9 "nine"
+                  10 "ten"
+                  11 "eleven"
+                  12 "twelve"
+                  13 "thirteen"
+                  14 "fourteen"
+                  15 "fifteen"
+                  16 "sixteen"
+                  17 "seventeen"
+                  18 "eighteen"
+                  19 "nineteen"})
 
 (defn number-to-words
   [n]
   (cond
     (< n 20) (small-words n)
     :default (let [tens (quot n 10)
-          units (rem n 10)]
-               (str (tens-words tens) (small-words  units)))
-    )
-  )
-
+                   units (rem n 10)]
+               (str (tens-words tens) (small-words  units)))))
 
 (number-to-words 3)
 (number-to-words 10)
@@ -54,46 +46,35 @@
 (defn pluralise [n s]
   (if (= n 1)
     s
-    (str s "s"))
-  )
+    (str s "s")))
 
 (defn express-mins
   [m]
-  (let [
-        x (if (<= m 30) m (- 60 m))
-        to-past (if (<= m 30) "past" "to")
-        ]
+  (let [x (if (<= m 30) m (- 60 m))
+        to-past (if (<= m 30) "past " "to ")]
     (cond
       (zero? x) nil
       (= 15 x) (str "quarter " to-past)
-      (= 30 x) "half past"
-      :def (str (number-to-words x) " " (pluralise x "minute") " " to-past )))
-  )
+      (= 30 x) "half past "
+      :def (str (number-to-words x) " " (pluralise x "minute") " " to-past))))
 
 (defn express-hours
   [h m]
-  (let [
-        reference-hour (if  (<= m 30) h (rem (inc h) 24))
+  (let [reference-hour (if  (<= m 30) h (rem (inc h) 24))
         x (if (<= reference-hour 12) reference-hour (- reference-hour 12))
         am-pm (if (< reference-hour 12) "am" "pm")]
     (cond
-      (= reference-hour 0 ) "midnight"
-      (= reference-hour 12 ) "noon"
-      :default (str (number-to-words x) " " am-pm))
-    )
-  )
+      (= reference-hour 0) "midnight"
+      (= reference-hour 12) "noon"
+      :default (str (number-to-words x) " " am-pm))))
 
 ;; 14:18 => eighteen minutes past 2 pm
 (defn time-sentence
   [time]
   (let [[hours mins] (mapv as-int  (clojure.string/split time #":"))
         minute-expression (express-mins mins)
-        hour-expression (express-hours hours mins)
-        ]
-    (str minute-expression " " hour-expression))
-
-  )
-
+        hour-expression (express-hours hours mins)]
+    (str minute-expression  hour-expression)))
 
 (time-sentence "14:30")
 
@@ -114,10 +95,22 @@
 (time-sentence "00:02")
 
 (time-sentence "23:59")
-
+(time-sentence "23:45")
 
 (defn -main
   [& args]
-  (println "Hello, World!")
-
+  (doseq [time ["00:00"
+              "14:30"
+              "14:15"
+              "14:45"
+              "14:18"
+              "14:48"
+              "14:00"
+              "12:00"
+              "12:01"
+              "00:00"
+              "00:02"
+              "23:59"
+              "23:45"]]
+    (println "at" time "we say" (time-sentence time))) 
   )
